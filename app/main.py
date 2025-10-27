27a96fdfe0a6fa87902866184f6ab3d9947ca1c6
Alex <alexx5039@gmail.com>
2025-10-27 12:25:39 +0200

    people dictionary task implemented

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    persons = [Person(person_data["name"], person_data["age"])
+               for person_data in people
+               ]
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        wife = person_data.get("wife")
+        husband = person_data.get("husband")
+
+        if wife:
+            person.wife = Person.people[wife]
+        elif husband:
+            person.husband = Person.people[husband]
+    return persons
